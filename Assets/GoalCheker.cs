dc861145644a06e2903f6ca6af76e4121df60eff
MrRyonk <mr.ryonk@gmail.com>
2021-01-01 23:06:40 +0900

    ゴール判定、バカゲーになる
    ゴール設置
    柵設置

---
+++ Assets/GoalCheker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalCheker : MonoBehaviour
+{
+    public GameObject quad2;
+
+    public Camera mainCamera;
+    public Camera subCamera;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "chara")
+        {
+            quad2.SetActive(false);
+            mainCamera.enabled = false;
+            subCamera.enabled = true;
+            text.SetActive(true);
+        }
+    }
+}
