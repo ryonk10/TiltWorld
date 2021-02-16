7ff9392958497f254c1b18ebd622ba18220fb7ba
MrRyonk <mr.ryonk@gmail.com>
2021-02-17 00:05:24 +0900

    メインステージのゴール処理追加

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject goalCanvas;
+   public void ShowGoalForm()
+    {
+        goalCanvas.SetActive(true);
+    }
+    public void CloseGoalForm()
+    {
+        goalCanvas.SetActive(false);
+    }
+}
