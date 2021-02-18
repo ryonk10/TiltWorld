953db56713c83c392fe54884d3a6973069b37de4
MrRyonk <mr.ryonk@gmail.com>
2021-02-18 23:07:27 +0900

    ライティング微調整

--- Assets/Script/CharaController.cs
+++ Assets/Script/CharaController.cs
@@ -35,7 +35,7 @@ public class CharaController : MonoBehaviour
         }
         else if (stageCon.stageFaze == stageController.StageFaze.GOAL)
         {
-            float step = 8 * Time.deltaTime;
+            float step = 3 * Time.deltaTime;
             this.rigi.velocity = new Vector3(1, 0, 5) * step;
         }
     }
